579638a089b7b8fd57999ea85e4ee17ace5e87ca
aguilarmelissa101 <54563452+aguilarmelissa101@users.noreply.github.com>
2019-09-23 16:43:26 -0400

    Lab 2: Flask, HTML and CSS
    This was my Lab 2 assignment. If the images do not appear on your computer please email me because I can send you screen shots that it does.

---
+++ LAB_2/lab2_part1_flask.py
@@ -0,0 +1,17 @@
+##Melissa Aguilar
+##Users are able to input anything at the end of the URL by inputting /user/name
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
+if __name__ == '__main__':
+    app.run()
+
