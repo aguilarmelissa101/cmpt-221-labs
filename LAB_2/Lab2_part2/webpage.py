b9b4a2a7ecede79f6cdd1780d82d17ce0dfb235b
aguilarmelissa101 <54563452+aguilarmelissa101@users.noreply.github.com>
2019-09-23 22:29:00 -0400

    added webpage to home and resume

--- LAB_2/Lab2_part2/webpage.py
+++ LAB_2/Lab2_part2/webpage.py
@@ -1,15 +1,16 @@
 ##Melissa Aguilar
 ##Users are able to input anything at the end of the URL by inputting /user/name
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/home')
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template("home_page.html")
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+@app.route('/resume')
+def resume():
+    return render_template("resume_page.html")
 
 if __name__ == '__main__':
     app.run()
