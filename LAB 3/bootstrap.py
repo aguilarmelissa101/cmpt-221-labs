d34e323dd573e0d1abd5fff1326d13c072086cb1
aguilarmelissa101 <54563452+aguilarmelissa101@users.noreply.github.com>
2019-09-29 17:07:52 -0400

    trying to connect to flask

---
+++ LAB 3/bootstrap.py
@@ -0,0 +1,15 @@
+##Melissa Aguilar
+##Users are able to input anything at the end of the URL by inputting /user/name
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
