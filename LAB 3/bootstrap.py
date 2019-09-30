c004aab79f49a70cfd00c409e02050961474c037
aguilarmelissa101 <54563452+aguilarmelissa101@users.noreply.github.com>
2019-09-30 17:51:21 -0400

    testing

--- LAB 3/bootstrap.py
+++ LAB 3/bootstrap.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 # Adding dynamic route with templates
-@app.route("/index")
+@app.route("/")
 def index():
     return render_template("index.html"), 200
 
@@ -15,7 +15,7 @@ def index():
 # Run Flask Programmatically
 if __name__ == "__main__":
     # Set Debug to true, set host IP to localhost, and set port to 80
-    app.run(debug=True, host="127.0.0.1", port=80)
+    app.run(debug=True, host="127.0.0.1")
 
 
 
