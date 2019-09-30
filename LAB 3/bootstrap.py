8b27b5a417e98f12bc4d7d48a9725f95471e947a
aguilarmelissa101 <54563452+aguilarmelissa101@users.noreply.github.com>
2019-09-30 12:35:09 -0400

    testing

--- LAB 3/bootstrap.py
+++ LAB 3/bootstrap.py
@@ -1,13 +1,17 @@
 ##Melissa Aguilar
 ##Users are able to input anything at the end of the URL by inputting /user/name
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
+@app.route('/#portfolio')
+def portfolio():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
